a1b7fe710cece2c3f32299ff527bf4c3c6961e1c
Ab3nXY <127937158+Ab3nXY@users.noreply.github.com>
2023-10-21 19:23:09 +0300

    Update SECRET_KEY

--- books/settings.py
+++ books/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*)vhi3g5x0z3m)__()%hf$s_w)i=#ozw6^l!4(yoy_xf9v8si1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
